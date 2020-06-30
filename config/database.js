ec2735d93a8d190f9d48cfccfa76ba26687a20c7
nguyenduy7474 <nguyenduy7474@gmail.com>
2020-06-30 18:28:48 +0700

    ver2

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://127.1.1.0/express' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-     
+    //'url' : 'mongodb://127.1.1.0/express' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     'url' : 'mongodb://nguyenduy7475:duy123654789@ds127376.mlab.com:27376/voicechat'
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
 };
